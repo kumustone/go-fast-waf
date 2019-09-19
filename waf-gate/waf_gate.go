2b02a50fcb256b805de10c44d0d52c5843fa8b35
kumu <liubo@zhuangdianwenhua.com>
2019-09-19 16:45:31 +0800

    增加https支持

--- waf-gate/waf_gate.go
+++ waf-gate/waf_gate.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
+	"errors"
 	"flag"
+	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/kumustone/panda-waf"
 	"github.com/natefinch/lumberjack"
@@ -13,6 +16,8 @@ import (
 
 type GateConfig struct {
 	GateHttpAddress  string
+	StartHttps       bool
+	CertKeyList      [][]string
 	GateHttpsAddress string
 	GateAPIAddress   string
 	UpstreamList     []string
@@ -24,7 +29,7 @@ type WafGateConfig struct {
 }
 
 var (
-	confFile = flag.String("c", "./conf/waf_gate.conf", "Config file")
+	confFile = flag.String("c", "./waf_gate.conf.template.toml", "Config file")
 	logPath  = flag.String("l", "./log", " log path")
 )
 
@@ -36,6 +41,8 @@ func main() {
 		log.Fatal("Can not decode config file ", err.Error())
 		return
 	}
+
+	fmt.Println(c)
 
 	defer panda_waf.PanicRecovery(true)
 
@@ -61,7 +68,6 @@ func main() {
 	}
 
 	panda_waf.UpStream.WaitNotify()
-
 	server := &http.Server{
 		Addr:           c.Gate.GateHttpAddress,
 		IdleTimeout:    3 * time.Minute,
@@ -71,6 +77,43 @@ func main() {
 		Handler:        panda_waf.WafHandler,
 	}
 
-	log.Fatal(server.ListenAndServe())
+	go server.ListenAndServe()
 
+	if c.Gate.StartHttps {
+
+		caData := make(map[string][]string)
+		for _, item := range c.Gate.CertKeyList {
+			if len(item) != 3 {
+				continue
+			}
+			caData[item[0]] = []string{item[1], item[2]}
+		}
+
+		cfg := &tls.Config{
+			GetCertificate: func(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
+				data, ok := caData[info.ServerName]
+				if !ok {
+					return nil, errors.New("Cert Key is not exist")
+				}
+				cert, err := tls.LoadX509KeyPair(data[0], data[1])
+				if err != nil {
+					return nil, err
+				}
+				return &cert, nil
+			},
+		}
+
+		server := http.Server{
+			IdleTimeout:    3 * time.Minute,
+			ReadTimeout:    5 * time.Minute,
+			WriteTimeout:   5 * time.Minute,
+			MaxHeaderBytes: 20 * 1024 * 1024,
+			Handler:        panda_waf.WafHandler,
+			TLSConfig:      cfg,
+		}
+		fmt.Println("Https start at ", c.Gate.GateHttpsAddress)
+		log.Fatal(server.ListenAndServeTLS("", ""))
+	}
+
+	select {}
 }
