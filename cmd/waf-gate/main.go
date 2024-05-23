dbc7d89097a1855bf654962b2ab84aa9105f6bd4
kumustone <kumustone@gmail.com>
2024-05-24 00:21:13 +0800

    Formatting changes

---
+++ cmd/waf-gate/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/tls"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	. "go-fast-waf/internal/gate"
+	. "go-fast-waf/internal/share"
+	lumberjack "gopkg.in/natefinch/lumberjack.v2"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
+
+type GateConfig struct {
+	GateHttpAddress  string
+	StartHttps       bool
+	CertKeyList      [][]string
+	GateHttpsAddress string
+	GateAPIAddress   string
+	UpstreamList     []string
+}
+
+type WafGateConfig struct {
+	Gate   GateConfig
+	WAFRPC Config
+}
+
+var (
+	confFile = flag.String("c", "./waf_gate.conf", "Config file")
+	logPath  = flag.String("l", "./log", " log path")
+)
+
+func main() {
+	flag.Parse()
+
+	c := WafGateConfig{}
+	if _, err := toml.DecodeFile(*confFile, &c); err != nil {
+		log.Fatal("Can not decode config file ", err.Error())
+		return
+	}
+
+	log.Println(c)
+
+	defer PanicRecovery(true)
+
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   *logPath + "/waf_gate.log",
+		MaxSize:    1,
+		MaxBackups: 10,
+		MaxAge:     30,
+	})
+
+	//go pprof检测
+	go func() {
+		log.Println(http.ListenAndServe("0.0.0.0:60060", nil))
+	}()
+
+	InitConfig(c.WAFRPC)
+	WaitServerNotify()
+
+	for _, it := range c.Gate.UpstreamList {
+		UpStream.Add(&RouterItem{
+			Key: it,
+		})
+	}
+
+	UpStream.WaitNotify()
+	server := &http.Server{
+		Addr:           c.Gate.GateHttpAddress,
+		IdleTimeout:    3 * time.Minute,
+		ReadTimeout:    5 * time.Minute,
+		WriteTimeout:   5 * time.Minute,
+		MaxHeaderBytes: 20 * 1024 * 1024,
+		Handler:        WafHandler,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			fmt.Println("Listen and serve error ", err.Error())
+		}
+	}()
+
+	if c.Gate.StartHttps {
+
+		caData := make(map[string][]string)
+		for _, item := range c.Gate.CertKeyList {
+			if len(item) != 3 {
+				continue
+			}
+			caData[item[0]] = []string{item[1], item[2]}
+		}
+
+		cfg := &tls.Config{
+			GetCertificate: func(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
+				data, ok := caData[info.ServerName]
+				if !ok {
+					return nil, errors.New("Cert Key is not exist")
+				}
+				cert, err := tls.LoadX509KeyPair(data[0], data[1])
+				if err != nil {
+					return nil, err
+				}
+				return &cert, nil
+			},
+		}
+
+		server := http.Server{
+			IdleTimeout:    3 * time.Minute,
+			ReadTimeout:    5 * time.Minute,
+			WriteTimeout:   5 * time.Minute,
+			MaxHeaderBytes: 20 * 1024 * 1024,
+			Handler:        WafHandler,
+			TLSConfig:      cfg,
+		}
+		fmt.Println("Https start at ", c.Gate.GateHttpsAddress)
+		log.Fatal(server.ListenAndServeTLS("", ""))
+	}
+
+	select {}
+}
